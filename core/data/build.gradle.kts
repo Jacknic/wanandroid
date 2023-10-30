8bd90c88107066adf282414f8eee13b3bca7d7c9
Jacknic <jacknic@qq.com>
2023-10-30 15:16:10 +0800

    feat: 添加 repo 默认实现及配置信息

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -7,13 +7,14 @@ android {
 }
 
 dependencies {
-    implementation(project(":core:analytics"))
-    implementation(project(":core:common"))
-    implementation(project(":core:database"))
-    implementation(project(":core:datastore"))
-    implementation(project(":core:model"))
-    implementation(project(":core:network"))
-    implementation(project(":core:notification"))
+    api(project(":core:analytics"))
+    api(project(":core:domain"))
+    api(project(":core:common"))
+    api(project(":core:database"))
+    api(project(":core:datastore"))
+    api(project(":core:model"))
+    api(project(":core:network"))
+    api(project(":core:notification"))
 
 //    implementation(libs.androidx.core.ktx)
 //    implementation(libs.kotlinx.coroutines.android)
---
+++ core/data/src/main/kotlin/com/jacknic/android/core/data/DefaultWanRepository.kt
@@ -0,0 +1,25 @@
+package com.jacknic.android.core.data
+
+import com.jacknic.android.core.domain.data.WanRepository
+import com.jacknic.android.core.model.Article
+import com.jacknic.android.core.model.Paging
+import com.jacknic.android.core.network.WanApi
+import com.jacknic.android.core.network.toResult
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * 数据读取默认实现类
+ *
+ * @author Jacknic
+ */
+@Singleton
+class DefaultWanRepository @Inject constructor(private val api: WanApi) : WanRepository {
+
+    override suspend fun getHomeArticleList(
+        page: Int,
+        pageSize: Int
+    ): Result<Paging<Article>> = runCatching {
+        return api.getArticleList(page, pageSize).toResult()
+    }
+}
