1b5de6f4b0b360e5529ed92841693da2977f0704
Jacknic <jacknic@qq.com>
2023-11-04 23:42:18 +0800

    feat: 添加并使用默认调试签名

--- build.gradle.kts
+++ build.gradle.kts
@@ -143,23 +143,21 @@ fun BaseAppModuleExtension.configApplication(target: Project) {
         dependencies {
             implementation(libs.core.ktx)
             implementation(libs.lifecycle.runtime.ktx)
-            implementation(libs.activity.compose)
-            implementation(platform(libs.compose.bom))
-            implementation(libs.ui)
-            implementation(libs.ui.graphics)
-            implementation(libs.ui.tooling.preview)
-            implementation(libs.material3)
-
             androidTestImplementation(libs.androidx.test.ext.junit)
             androidTestImplementation(libs.espresso.core)
-            androidTestImplementation(platform(libs.compose.bom))
-            androidTestImplementation(libs.ui.test.junit4)
-
-            debugImplementation(libs.ui.tooling)
-            debugImplementation(libs.ui.test.manifest)
         }
     }
     compileSdk = VersionCodes.TIRAMISU
+
+    signingConfigs {
+        getByName("debug") {
+            keyAlias = "debug"
+            storePassword = "123456"
+            keyPassword = "123456"
+            storeFile = rootProject.file("debug.jks")
+        }
+    }
+
     defaultConfig {
         minSdk = VersionCodes.LOLLIPOP
         targetSdk = VersionCodes.TIRAMISU
@@ -182,12 +180,6 @@ fun BaseAppModuleExtension.configApplication(target: Project) {
         }
     }
 
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
