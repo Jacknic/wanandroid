ae5c987b23b87a13fb541c3c2f6bb4ee96ffd828
Jacknic <jacknic@qq.com>
2025-03-05 01:10:41 +0800

    feat: 添加二级页面导航

--- app/src/main/java/com/jacknic/android/wanandroid/ui/page/main/tree/PageTree.kt
+++ app/src/main/java/com/jacknic/android/wanandroid/ui/page/main/tree/PageTree.kt
@@ -1,21 +1,63 @@
 package com.jacknic.android.wanandroid.ui.page.main.tree
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
+data class ContentItem(val text: String)
+
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
 @Composable
 fun PageTree() {
-    Scaffold {
-        Column(Modifier.padding(it)) {
-            Text("体系页面")
+    val scaffoldNavigator = rememberListDetailPaneScaffoldNavigator<ContentItem>()
+    ListDetailPaneScaffold(
+        directive = scaffoldNavigator.scaffoldDirective,
+        value = scaffoldNavigator.scaffoldValue,
+        listPane = {
+            AnimatedPane(
+                modifier = Modifier
+                    .preferredWidth(200.dp)
+                    .systemBarsPadding(),
+            ) {
+                Surface {
+                    LazyColumn {
+                        items(100) {
+                            val text = "List item $it"
+                            ListItem(
+                                headlineContent = { Text(text) },
+                                modifier = Modifier.clickable {
+                                    scaffoldNavigator.navigateTo(
+                                        ListDetailPaneScaffoldRole.Detail,
+                                        ContentItem(text)
+                                    )
+                                })
+                        }
+                    }
+                }
+            }
+        },
+        detailPane = {
+            val content = scaffoldNavigator.currentDestination?.content
+            if (content != null) {
+                AnimatedPane(modifier = Modifier.systemBarsPadding()) {
+                    Surface(
+                        onClick = { scaffoldNavigator.navigateBack() }
+                    ) {
+                        Text("Details => ${content.text}")
+                    }
+                }
+            }
         }
-    }
+    )
 }
