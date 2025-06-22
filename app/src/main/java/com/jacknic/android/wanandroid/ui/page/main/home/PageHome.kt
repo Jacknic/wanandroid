1c5a7a53963fbd93dd4805dc764091b373c09556
Jacknic <jacknic@qq.com>
2025-06-22 17:42:49 +0800

    feat: 调整首页内容显示

--- app/src/main/java/com/jacknic/android/wanandroid/ui/page/main/home/PageHome.kt
+++ app/src/main/java/com/jacknic/android/wanandroid/ui/page/main/home/PageHome.kt
@@ -2,7 +2,10 @@ package com.jacknic.android.wanandroid.ui.page.main.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,16 +16,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.twotone.Search
+import androidx.compose.material.icons.twotone.AccountCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.PrimaryScrollableTabRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SecondaryScrollableTabRow
 import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -31,18 +36,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.jacknic.android.wanandroid.core.common.getDataOrNull
+import com.jacknic.android.wanandroid.core.design.components.PreviewTabItem
 import com.jacknic.android.wanandroid.ui.component.ArticleListItem
 import com.jacknic.android.wanandroid.ui.component.HomeBanner
+import com.jacknic.android.wanandroid.ui.component.SearchBar
 import com.jacknic.android.wanandroid.ui.page.LocalNavCtrl
 import com.jacknic.android.wanandroid.ui.page.Page
 
@@ -66,12 +75,18 @@ fun PageHome(
     var tabIndex by rememberSaveable { mutableIntStateOf(0) }
     Scaffold(topBar = {
         TopAppBar(
-            title = {},
+            title = {
+                SearchBar(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = { nav.navigate(Page.Search) }
+                )
+            },
             actions = {
+                Spacer(Modifier.size(8.dp))
                 IconButton(onClick = {
                     nav.navigate(Page.Search)
                 }) {
-                    Icon(Icons.TwoTone.Search, "搜索")
+                    Icon(Icons.TwoTone.AccountCircle, "签到")
                 }
             },
             scrollBehavior = scrollBehavior
@@ -86,24 +101,39 @@ fun PageHome(
             state = listStateTop
         ) {
             stickyHeader {
-                PrimaryScrollableTabRow(
-                    tabIndex,
-                    modifier = Modifier.fillParentMaxWidth(),
-                    edgePadding = 8.dp,
-                    divider = {}
-                ) {
-                    for (i in 0 until 15) {
-                        Tab(tabIndex == i, onClick = {
-                            tabIndex = i
-                        }) {
-                            Text(
-                                "Tab内容 $i",
-                                modifier = Modifier
-                                    .align(Alignment.CenterHorizontally)
-                                    .padding(12.dp, 8.dp)
+                Column(modifier = Modifier.background(MaterialTheme.colorScheme.surface)) {
+                    SecondaryScrollableTabRow(
+                        selectedTabIndex = tabIndex,
+                        modifier = Modifier.fillParentMaxWidth(),
+                        edgePadding = 8.dp,
+                        containerColor = Color.Unspecified,
+                        divider = {},
+                        indicator = {
+                            TabRowDefaults.PrimaryIndicator(
+                                modifier = Modifier.tabIndicatorOffset(tabIndex),
+                                width = 16.dp,
                             )
                         }
+                    ) {
+                        for (i in 0 until 15) {
+                            val interactionSource = remember { MutableInteractionSource() }
+                            Tab(
+                                selected = tabIndex == i,
+                                onClick = { tabIndex = i },
+                                modifier = Modifier.indication(interactionSource, null),
+                                unselectedContentColor = LocalContentColor.current.copy(0.6f),
+                                interactionSource = interactionSource
+                            ) {
+                                Text(
+                                    text = "Tab内容 $i",
+                                    modifier = Modifier
+                                        .align(Alignment.CenterHorizontally)
+                                        .padding(12.dp, 8.dp)
+                                )
+                            }
+                        }
                     }
+                    PreviewTabItem()
                 }
             }
             item {
