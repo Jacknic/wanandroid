9c1a45183c629f7d7c759a17ced27b09f033e604
Jacknic <jacknic@qq.com>
2025-06-22 18:18:47 +0800

    feat: 简化结构移除水波纹效果

--- app/src/main/java/com/jacknic/android/wanandroid/ui/page/main/home/PageHome.kt
+++ app/src/main/java/com/jacknic/android/wanandroid/ui/page/main/home/PageHome.kt
@@ -2,8 +2,7 @@ package com.jacknic.android.wanandroid.ui.page.main.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.indication
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -26,7 +25,6 @@ import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SecondaryScrollableTabRow
-import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -36,7 +34,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -116,21 +113,19 @@ fun PageHome(
                         }
                     ) {
                         for (i in 0 until 15) {
-                            val interactionSource = remember { MutableInteractionSource() }
-                            Tab(
-                                selected = tabIndex == i,
-                                onClick = { tabIndex = i },
-                                modifier = Modifier.indication(interactionSource, null),
-                                unselectedContentColor = LocalContentColor.current.copy(0.6f),
-                                interactionSource = interactionSource
-                            ) {
-                                Text(
-                                    text = "Tab内容 $i",
-                                    modifier = Modifier
-                                        .align(Alignment.CenterHorizontally)
-                                        .padding(12.dp, 8.dp)
-                                )
-                            }
+                            val alpha = if (i == tabIndex) 1f else 0.6f
+                            val color = LocalContentColor.current.copy(alpha)
+                            Text(
+                                text = "Tab内容 $i",
+                                color = color,
+                                modifier = Modifier
+                                    .align(Alignment.CenterHorizontally)
+                                    .clickable(
+                                        interactionSource = null,
+                                        indication = null
+                                    ) { tabIndex = i }
+                                    .padding(12.dp, 8.dp)
+                            )
                         }
                     }
                     PreviewTabItem()
