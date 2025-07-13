f33a599b6a6f960d6e8924f09ce4011e97097c8a
Jacknic <jacknic@qq.com>
2025-07-13 15:09:06 +0800

    feat: 修改浏览器页面样式及生命周期状态监听

--- app/src/main/java/com/jacknic/android/wanandroid/ui/page/browser/PageBrowser.kt
+++ app/src/main/java/com/jacknic/android/wanandroid/ui/page/browser/PageBrowser.kt
@@ -5,6 +5,7 @@ import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -20,12 +21,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.os.bundleOf
 import com.jacknic.android.wanandroid.ui.page.LocalNavCtrl
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,19 +44,16 @@ fun PageBrowser() {
     val backStackEntry = nav.previousBackStackEntry
     val savedStateHandle = backStackEntry?.savedStateHandle
     val link = savedStateHandle?.get<String?>("link")
-    var webView: WebView? = null
-    val titleState = remember { mutableStateOf("") }
-    val progress = remember { mutableFloatStateOf(0f) }
+    var titleState by remember { mutableStateOf("") }
+    var progress by remember { mutableFloatStateOf(0f) }
+    var launchUrl by rememberSaveable { mutableStateOf<String?>(null) }
+    val saveBundle = rememberSaveable { bundleOf() }
 
     Scaffold(topBar = {
         TopAppBar(
             navigationIcon = {
                 IconButton(onClick = {
-                    if (webView?.canGoBack() == true) {
-                        webView?.goBack()
-                    } else {
-                        nav.navigateUp()
-                    }
+                    nav.navigateUp()
                 }) {
                     Icon(Icons.AutoMirrored.TwoTone.ArrowBack, "")
                 }
@@ -61,43 +65,37 @@ fun PageBrowser() {
             },
             title = {
                 Text(
-                    titleState.value,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
+                    titleState, maxLines = 1, overflow = TextOverflow.Ellipsis
                 )
-            })
+            }
+        )
     }) { padding ->
         Box(Modifier.padding(padding)) {
+            val webView = rememberWebViewWithState(
+                onTitleChange = { titleState = it ?: "" },
+                onLoadChange = { progress = it })
             AndroidView(
-                factory = { ctx ->
-                    WebView(ctx).also { webView = it }
-                }, modifier = Modifier.fillMaxSize(1f)
-            ) { web ->
-                val settings = web.settings
-                settings.javaScriptEnabled = true
-                settings.domStorageEnabled = true
-                // webView.width = LayoutParams.MATCH_PARENT
-                web.webViewClient = object : WebViewClient() {
-                    override fun shouldOverrideUrlLoading(
-                        view: WebView,
-                        request: WebResourceRequest
-                    ): Boolean {
-                        val url = request.url.toString()
-                        return !url.startsWith("http", true)
-                    }
+                factory = { webView },
+                modifier = Modifier.fillMaxSize(1f),
+                onRelease = {
+                    it.saveState(saveBundle)
+                    launchUrl = it.url
+                    it.stopLoading()
+                },
+                onReset = {
+                    it.stopLoading()
+                    it.loadUrl("about:blank")
+                },
+                update = {}
+            )
+            LaunchedEffect(Unit) {
+                if (saveBundle.isEmpty) {
+                    webView.loadUrl(launchUrl ?: link.toString())
+                } else {
+                    webView.restoreState(saveBundle)
                 }
-                web.webChromeClient = object : WebChromeClient() {
-                    override fun onReceivedTitle(view: WebView?, title: String?) {
-                        titleState.value = title.toString()
-                    }
-
-                    override fun onProgressChanged(view: WebView?, newProgress: Int) {
-                        progress.floatValue = newProgress / 100f
-                    }
-                }
-                web.loadUrl(link.toString())
             }
-            val progressValue = progress.floatValue
+            val progressValue = progress
             if (progressValue < 1) {
                 LinearProgressIndicator(
                     progress = { progressValue },
@@ -106,4 +104,44 @@ fun PageBrowser() {
             }
         }
     }
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun rememberWebViewWithState(
+    onTitleChange: (String?) -> Unit = {},
+    onLoadChange: (Float) -> Unit = {},
+): WebView {
+    val context = LocalContext.current
+    val webView = remember {
+        WebView(context).apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(
+                    view: WebView, request: WebResourceRequest
+                ): Boolean {
+                    val url = request.url.toString()
+                    return !url.startsWith("http", true)
+                }
+            }
+            webChromeClient = object : WebChromeClient() {
+                override fun onReceivedTitle(view: WebView, title: String?) {
+                    if (view.url != title) {
+                        onTitleChange(title)
+                    }
+                }
+
+                override fun onProgressChanged(view: WebView, newProgress: Int) {
+                    onLoadChange(newProgress / 100f)
+                }
+            }
+        }
+    }
+
+    BackHandler(enabled = webView.canGoBack()) {
+        webView.goBack()
+    }
+
+    return webView
 }
