2b233b8a904faa6e7cbd69f5204c579a2b808054
Jacknic <jacknic@qq.com>
2026-05-10 18:39:38 +0800

    feat: 添加网络异常类型判断方法

--- core/network/src/main/kotlin/com/jacknic/android/wanandroid/core/network/WanServerException.kt
+++ core/network/src/main/kotlin/com/jacknic/android/wanandroid/core/network/WanServerException.kt
@@ -1,5 +1,7 @@
 package com.jacknic.android.wanandroid.core.network
 
+import com.jacknic.android.wanandroid.core.model.WanResult
+
 /**
  * 玩安卓服务异常
  *
@@ -8,4 +10,21 @@ package com.jacknic.android.wanandroid.core.network
  *
  * @author Jacknic
  */
-class WanServerException(val code: Int, val msg: String = "") : Exception(msg)
+class WanServerException(val code: Int, val msg: String = "") : Exception(msg) {
+    fun isUnauthorized() = code == WanResult.ERROR_CODE_UNAUTHORIZED
+    fun isError() = code == WanResult.ERROR_CODE_ERROR
+}
+
+/**
+ * 判断异常是否为未登录
+ *
+ * @return true 是，false 否
+ */
+fun Throwable?.isUnauthorized() = this is WanServerException && this.isUnauthorized()
+
+/**
+ * 判断异常是否为错误
+ *
+ * @return true 是，false 否
+ */
+fun Throwable?.isError() = this is WanServerException && this.isError()
